5bd08b7530a3eb4afc7e19eb3df16545dedb107c
mareya83 <74158744+mareya83@users.noreply.github.com>
2023-03-17 01:07:55 +0200

    HomeWork5

--- Lessons/Lesson5/HomeWork.py
+++ Lessons/Lesson5/HomeWork.py
@@ -3,3 +3,59 @@
 
 # 1) Registration
 # 2) Autorizaetin -> greating
+
+data_base = []
+
+def menu():
+    menu_of_program = ["1) Registration", "2) Autorisation", "3) Data of users", "4) STOP the program"]
+
+    for i in menu_of_program:
+        print(i)
+    selection = int(input("Select a menu item: "))
+
+    if selection >= 1 and selection <= 4:
+        return selection
+    else:
+        print("\n Not a correct choice! Try again! \n")
+        menu()
+
+
+def data_of_user():
+    user = {"Login" : input("Enter your login: "),
+            "Password": input("Enter your password: ")}
+    if "@" "." in user["Login"] and user["Password"] > 2:
+         return user
+    else:
+        data_of_user()
+
+
+def registratin():
+    global data_base
+    
+    is_runing = True
+    while is_runing:
+
+        data_base.append(data_of_user())
+        is_runing = int(input("Would you like to create an account again?:  0 - Not, 1-Yes  "))
+
+    return data_base
+
+is_runing = True
+while is_runing:
+    selected = menu()
+    if selected == 1:
+        registratin()
+
+    elif selected == 2:
+        if data_of_user() in data_base:
+            print("Hello! \n")
+        else:
+            print("There is no such user \n")
+
+    elif selected == 3:
+        for i in data_base:
+            print(i)
+
+    else:
+        print("Goodby!")
+        is_runing = False
